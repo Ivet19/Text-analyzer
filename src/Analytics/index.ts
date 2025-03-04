fdc21fd4294c53c6a9818a2b6f66988cb1f9faa1
Ivet Lopez <ivet19@live.com>
2025-03-05 00:04:09 +0100

    Create getWordsTotal function and creare the functions to test it at tests_getWordsTotal.ts

--- src/Analytics/index.ts
+++ src/Analytics/index.ts
@@ -7,3 +7,15 @@ export const getParagraphsTotal = (text: string): number => {
 
   return paragraphs.length;
 };
+
+export const getWordsTotal = (text: string): number => {
+  const trimmedText = text.replaceAll("\n", " ");
+
+  const unfilteredWords = trimmedText.split(" ");
+
+  const words = unfilteredWords.filter(
+    (unfilteredWord) => unfilteredWord !== "" && unfilteredWord !== "\n"
+  );
+
+  return words.length;
+};
